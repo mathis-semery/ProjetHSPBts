23d2ea4993c225fd9d436c1ecd30d6d0a4d91a2d
leomnes <leomnes25@gmail.com>
2025-09-30 12:25:25 +0200

    feat template coté connexion / inscription fait (doit oui ou non enlever les conditions d'utilisation)

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,29 +16,25 @@ use Symfony\Component\Routing\Attribute\Route;
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $em): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            /** @var string $plainPassword */
-            $plainPassword = $form->get('plainPassword')->getData();
+            $user->setPassword(
+                $hasher->hashPassword($user, $form->get('plainPassword')->getData())
+            );
+            $em->persist($user);
+            $em->flush();
 
-            // encode the plain password
-            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            // do anything else you need here, like send an email
-
-            return $security->login($user, UserAuthenticator::class, 'main');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
         ]);
     }
+
 }

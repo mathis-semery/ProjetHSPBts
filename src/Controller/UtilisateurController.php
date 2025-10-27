02906365100b952ee3986f9152418f6e88c0c7c3
Mathis Semery <m.semery@lprs.fr>
2025-10-27 21:04:53 +0100

    fit/Fix "cryptage du mdp dans l edit du profile "

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -10,12 +10,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/utilisateur')]
 class UtilisateurController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile', methods: ['GET', 'POST'])]
-    public function profile(Request $request, EntityManagerInterface $em): Response
+    public function profile(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
 {
     $user = $this->getUser();
 
@@ -29,6 +30,13 @@ class UtilisateurController extends AbstractController
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
+        $plainPassword = $form->get('password')->getData();
+
+
+        if (!empty($plainPassword)) {
+            $hashed = $passwordHasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashed);
+        }
         $em->flush();
         $this->addFlash('success', 'Profil mis à jour avec succès.');
         return $this->redirectToRoute('app_profile');

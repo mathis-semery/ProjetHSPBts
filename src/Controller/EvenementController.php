03fcc9dba7e95a6046e3f77f9837cfcf3fa79a3f
Elias <zohrielias95@gmail.com>
2025-10-28 10:12:55 +0100

    [feat] Gestion Evenement ameliorer car il y avais des petit problemes de redirection et c'etais moche donc j'ai ameliorer sa

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -36,7 +36,7 @@ final class EvenementController extends AbstractController
             return $this->redirectToRoute('app_evenement_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('evenement/newregister.html.twig', [
+        return $this->render('evenement/new.html.twig', [
             'evenement' => $evenement,
             'form' => $form,
         ]);
